105e22aed4d6d55494d4b1572530e4f1de1e3b13
Каснер Герман <Axelchannel933@gmail.com>
2021-12-03 20:40:35 +0300

    Added new folders + task's

---
+++ Homework/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Вывод индекса минимального элемента массива 
+
+//                0  1  2  3   4  5   - индексы массива
+int[] massive = { 1, 2, 2, -4, 1, 6 };
+int Mass(int[] mass)
+{
+    int j = 0;
+    int min = massive[0];
+    for (int k = 0; k < massive.Length; k++) // пробежка по всей длине массива
+    {
+        if (massive[k] < min) // поиск минимума
+        {
+            min = massive[k];
+            j = k;  // Создаем ячейку, в которую кладём индекс минимального элемента 
+        }
+    }
+    return j;
+}
+int result = Mass(massive);
+Console.WriteLine(result);
--- Homework/Task5/Program.cs
+++ Homework/Task5/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//4. По заданному номеру дня недели вывести его название
+// 1 способ
+void Day(int a)
+{
+    if (a == 1) Console.WriteLine("Monday");
+    if (a == 2) Console.WriteLine("tuesday");
+    if (a == 3) Console.WriteLine("wednesday");
+    if (a == 4) Console.WriteLine("thursday");
+    if (a == 5) Console.WriteLine("friday");
+    if (a == 6) Console.WriteLine("saturday");
+    if (a == 7) Console.WriteLine("Sunday");
+}
+Day(4);
+
+//  2 способ 
+// void Day(int a)
+// {
+//     string[] Array = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+//     Console.WriteLine(Array[a - 1]);
+// }
+// Day(7);
