d72dffb265a1b82e3fed97c8dd7310bbbc89ed8e
Каснер Герман <Axelchannel933@gmail.com>
2021-12-03 20:25:47 +0300

    Added folders for task's

---
+++ Homework/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести квадрат числа
+// 1 cпособ 
+int Sqrt(int x)
+{
+    return x * x; // Возвращаем квадрат как произведение
+}
+
+int result = Sqrt(5);
+Console.WriteLine(result);
+// 2 cпособ 
+void Sqrt(int a)
+{
+    int S;
+    S = a * a;
+    Console.WriteLine(S);
+}
+
+Sqrt(5);
---
+++ Homework/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿//3. Даны два числа. Показать большее и меньшее число
+
+void Method(int a, int b)
+{
+    if (a > b) Console.WriteLine($"Число {a} - большее, число {b} - меньшее");
+    else Console.WriteLine($"Число {b} - большее, число {a} - меньшее");
+}
+Method(8, 3);
+Method(7, 10);
