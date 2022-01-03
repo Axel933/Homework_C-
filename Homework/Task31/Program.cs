b295c46879270a480abf364f28a58149cd853b95
Каснер Герман <Axelchannel933@gmail.com>
2022-01-03 20:43:28 +0300

    Task 31

---
+++ Homework/Task31/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[5, 5];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+         Console.WriteLine();
+    }
+}
+
+int[,] array=FillArray(5,5);
+PrintArray(array);
