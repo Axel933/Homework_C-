5bc30f30832d674787ac371468b5b416f386ddbc
Каснер Герман <Axelchannel933@gmail.com>
2022-01-03 20:43:17 +0300

    Task 30

---
+++ Homework/Task30/Program.cs
@@ -0,0 +1,45 @@
+﻿//В двумерном массиве n×k заменить четные элементы на противоположные
+
+int[,] FillArray(int n, int k)
+{
+    int[,] array = new int[n, k];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write($"{a[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ReverseArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0)
+            {
+                array[i, j] = array[i,j] *-1;
+            }
+        }
+    }
+    return array;
+}
+
+
+int[,] array = FillArray(5, 5);
+PrintArray(array);
+int[,] result= ReverseArray(array);
+Console.WriteLine();
+PrintArray(result);
