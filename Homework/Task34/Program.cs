6b7073087e5e4b177267582bad4d92adf60d6e80
Каснер Герман <Axelchannel933@gmail.com>
2022-01-04 20:41:55 +0300

    Task 34

---
+++ Homework/Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿//В матрице чисел найти сумму элементов главной диагонали
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write($"{a[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumMainDiagonal(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) sum = sum + array[i, j];
+        }
+    }
+    return sum;
+}
+
+int[,] array = FillArray(5, 5);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(SumMainDiagonal(array));
